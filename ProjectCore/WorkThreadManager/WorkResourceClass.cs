396116d429cad28c8e69ebed81b31ae2d8fcf979
taktakgyun <vbmrkk@naver.com>
2022-05-16 19:14:53 +0900

    WorkResourceClass add class MainCategory
    need change class
    subcategory

--- ProjectCore/WorkThreadManager/WorkResourceClass.cs
+++ ProjectCore/WorkThreadManager/WorkResourceClass.cs
@@ -1,95 +1,237 @@
-﻿namespace Mainboard
-{
-	using System;
-	using Netonsoft.Json;
-	using System.Collections.Generic;
+﻿
+using System;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
 
+namespace Mainboard
+{
 	/// <summary>
 	/// work resouce의 내용을 담을 수 있는 container
 	/// </summary>
-	interface IJObjectUseAbleContainer
-    {
-		public JObject GetJObject();
-		public void SetJOjbect(JObject obj);
-		public string GetKey();
-    }
+	public interface IJObjectUseAbleContainer
+	{
+		JObject GetJObject();
+		void SetJObject(JObject obj);
+		string GetKey();
+		JToken GetValue();
 
-	abstract class CompoundContainer :IJObjectUseAbleContainer
-    {
-		abstract public string GetKey();
+	}
+
+	public abstract class CompoundContainer : IJObjectUseAbleContainer
+	{
 		abstract public void SetJObject(JObject obj);
+		abstract public string GetKey();
 		public JObject GetJObject()
-        {
-			JObject obj = new JObject();
-			foreach(IJObjectUseAbleContainer container in containers)
+		{
+			JObject value = new JObject();
+			foreach (IJObjectUseAbleContainer container in containers)
 			{
-				obj.Add(container.GetKey(), container.GetJObject()[container.GetKey()]);
-            }
+				value.Add(container.GetKey(), container.GetJObject()[container.GetKey()]);
+			}
 			JObject json = new JObject();
-			json.Add(GetKey(), obj);
+			json.Add(GetKey(), value);
 			return json;
-        }
-		protected List<IJObjectUseAbleContainer> containers;
-    }
+		}
+		public void SetAtribute(IJObjectUseAbleContainer container)
+		{
+			containers.Add(container);
+		}
 
-	class ClassficationContainer : IJObjectUseAbleContainer
-    {
-		
-		public ClassficationContainer(string classfication=null)
-        {
+		public JToken GetValue()
+		{
+			JObject value = new JObject();
+			foreach (IJObjectUseAbleContainer container in containers)
+			{
+				value.Add(container.GetKey(), container.GetJObject()[container.GetKey()]);
+			}
+			return value;
+		}
+
+		protected List<IJObjectUseAbleContainer> containers = new List<IJObjectUseAbleContainer>();
+	}
+
+	public class ClassficationContainer : IJObjectUseAbleContainer
+	{
+		public ClassficationContainer(string classfication = "None")
+		{
 			this.classfication = classfication;
-        }
+		}
 		public string GetKey() { return "classfication"; }
 		public JObject GetJObject()
-        {
-			if (classfication == null) throw NullReferenceException;
+		{
+			//if (classfication == null)
 			JObject json = new JObject();
 			json.Add(GetKey(), classfication);
 			return json;
-        }
+		}
 		public void SetJObject(JObject obj)
-        {
-			classfication = obj[GetKey()];
+		{
+			classfication = (string)obj[GetKey()];
 		}
 		public void SetClassfication(string classfication)
-        {
+		{
 			this.classfication = classfication;
-        }
+		}
+
+		public JToken GetValue()
+		{
+			return GetJObject()[GetKey()];
+		}
+
 		protected string classfication;
 	}
 
-	class BoundBoxContainer : IJObjectUseAbleContainer
-    {
-		public BoundBoxContainer(int x_min=0, int x_max=0, int y_min=0, int y_max=0)
-        {
+	public class ConfidenceContainer : IJObjectUseAbleContainer
+	{
+		private float threashold = 0.55f;
+		public ConfidenceContainer(float confidence = 0)
+		{
+			this.confidence = (threashold <= confidence);
+		}
+
+		public JObject GetJObject()
+		{
+			JObject json = new JObject();
+			json.Add(GetKey(), confidence);
+			return json;
+		}
+
+		public string GetKey()
+		{
+			return "confidence";
+		}
+
+		public JToken GetValue()
+		{
+			return GetJObject()[GetKey()];
+		}
+
+		public void SetJObject(JObject obj)
+		{
+			confidence = (bool)obj[GetKey()];
+		}
+		public void SetConfidence(float confidence)
+		{
+			this.confidence = (threashold <= confidence);
+		}
+		bool confidence;
+	}
+
+	public class BoundBoxContainer : IJObjectUseAbleContainer
+	{
+		public BoundBoxContainer(int x_min = 0, int x_max = 0, int y_min = 0, int y_max = 0)
+		{
 			SetBoundBox(x_min, x_max, y_min, y_max);
 		}
 		public JObject GetJObject()
 		{
-			if (boundbox[0]+ boundbox[1]+ boundbox[2]+ boundbox[3] =< 0) throw NullReferenceException;
+			//if (boundbox[0]+ boundbox[1]+ boundbox[2]+ boundbox[3] =< 0) throw NullReferenceException;
 			JObject json = new JObject();
 			json.Add(GetKey(), boundbox);
 			return json;
 		}
 		public void SetJObject(JObject obj)
 		{
-			 = obj[GetKey()];
+			JArray value = (JArray)obj[GetKey()];
+			this.boundbox = value;
 		}
 		public void SetBoundBox(int x_min, int x_max, int y_min, int y_max)
-        {
-			this.boundbox[0] = x_min;
-			this.boundbox[1] = x_max;
-			this.boundbox[2] = y_min;
-			this.boundbox[3] = y_max;
+		{
+			boundbox = new JArray();
+			this.boundbox.Add(x_min);
+			this.boundbox.Add(x_max);
+			this.boundbox.Add(y_min);
+			this.boundbox.Add(y_max);
 		}
 
 		public string GetKey() { return "boundbox"; }
 
+		public JToken GetValue()
+		{
+			return GetJObject()[GetKey()];
+		}
+
 		/// <summary>
-        /// 0:x_min / 1:x_max / 2:y_min / 3:y_max
-        /// </summary>
-		protected int[4] boundbox;
-    }
+		/// 0:x_min / 1:x_max / 2:y_min / 3:y_max
+		/// </summary>
+		protected JArray boundbox;
+	}
+	public class MainCategoryContainer : CompoundContainer
+	{
+		public MainCategoryContainer()
+		{
+			key = "maincategory";
+			classficationContainer = new ClassficationContainer();
+			boundboxContainer = new BoundBoxContainer();
+			//SetAtribute(classficationContainer);
+			SetAtribute(boundboxContainer);
+		}
+		public override string GetKey()
+		{
+			return key;
+		}
+		public void SetKey()
+		{
+			key = classficationContainer.GetValue().ToString();
+		}
+		public override void SetJObject(JObject obj)
+		{
+			JObject value = (JObject)obj[key];
+			foreach (IJObjectUseAbleContainer container in containers)
+			{
+				container.SetJObject(value);
+			}
+			SetKey();
+		}
+		public void SetClassfication(string classfication)
+		{
+			this.classficationContainer.SetClassfication(classfication);
+			SetKey();
+		}
+		public void SetBoundbox(int x_min, int x_max, int y_min, int y_max)
+		{
+			boundboxContainer.SetBoundBox(x_min, x_max, y_min, y_max);
+		}
+
+		ClassficationContainer GetClassficationContainer()
+		{
+			return classficationContainer;
+		}
+		BoundBoxContainer GetBoundBoxContainer()
+		{
+			return boundboxContainer;
+		}
+
+
+		string key;
+		public ClassficationContainer classficationContainer;
+		public BoundBoxContainer boundboxContainer;
+	}
+
+	public class SubCategoryContainer : CompoundContainer
+	{
+		public SubCategoryContainer()
+		{
+			SetAtribute(classficationContainer);
+			SetAtribute(confidenceContainer);
+		}
+		public override string GetKey()
+		{
+			return "subcategory";
+		}
+
+		public override void SetJObject(JObject obj)
+		{
+			JObject value = (JObject)obj[GetKey()];
+			foreach (IJObjectUseAbleContainer container in containers)
+			{
+				container.SetJObject(value);
+			}
+		}
+		//string key;
+		public ClassficationContainer classficationContainer = new ClassficationContainer();
+		public ConfidenceContainer confidenceContainer = new ConfidenceContainer();
+	}
 	public class WorkResourceClass
 	{
 		public WorkResourceClass()
